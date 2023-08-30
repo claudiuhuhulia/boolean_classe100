72fdb2f5ee93b29be40532c6597b1552fdbd286b
marcocareddu <saldumaccu@tuta.io>
2023-08-30 12:18:24 +0200

    CREATE - Added create blade

---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('main')
+    <div class="container">
+
+
+        <div class="container text-center">
+
+
+            <form method="POST" action="{{ route('students.store') }}">
+
+                {{-- Token --}}
+                @csrf
+
+                <div class="row justify-content-center">
+                    <h1 class="mb-3">ADD Student</h1>
+
+                    {{-- Nome Studente --}}
+                    <div class="mb-3 col-6">
+                        <label class="text-start" for="name">Nome</label>
+                        <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}"
+                            autofocus>
+                    </div>
+
+                    {{-- Cognome Studente --}}
+                    <div class="mb-3 col-6">
+                        <label class="form-label" for="surname">Cognome</label>
+                        <input type="text" id="surname" name="surname" class="form-control"
+                            value="{{ old('surname') }}">
+                    </div>
+
+                </div>
+                <button class="btn btn-success">Add</button>
+            </form>
+
+        </div>
+
+    </div>
+@endsection
