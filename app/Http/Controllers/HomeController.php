3065fc892540ae1fe09e1fafcfa50823de544753
marcocareddu <saldumaccu@tuta.io>
2023-08-30 17:15:05 +0200

    Added validation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,4 +15,35 @@ class HomeController extends Controller
         $students = Student::all();
         return view('home', compact('students'));
     }
+
+    // Store Comic
+    public function store(Request $request)
+    {
+        // VALIDATION
+        $request->validate([
+            'title' => 'required|string',
+            'thumb' => 'required|string',
+        ]);
+
+        $data = $request->all();
+        $student = new Student;
+        $student->fill($data);
+        $student->save();
+
+        return to_route('students.show', $student);
+    }
+
+    // Store Comic
+    public function update(Request $request, Student $student)
+    {
+        // VALIDATION
+        $request->validate([
+            'title' => 'required|string',
+            'thumb' => 'required|string',
+        ]);
+
+        $data = $request->all();
+        $student->update($data);
+        return to_route('students.show', $student);
+    }
 }
