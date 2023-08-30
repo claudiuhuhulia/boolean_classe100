76c3d5350182d6d3501303e37eb24127f4756771
marcocareddu <saldumaccu@tuta.io>
2023-08-30 12:40:34 +0200

    Added create template

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\Request;
 
 class StudentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+
+    // Create Students
+    public function create()
     {
-        //
+        return view('students.create');
+    }
+
+    // Store Comic
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+        $student = new Student;
+        $student->fill($data);
+        $student->save();
+
+        return to_route('students.show', $student);
     }
 }
--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,39 +1,33 @@
 @extends('layouts.main')
 
 @section('main')
-    <div class="container">
+    <div class="container text-center">
 
 
-        <div class="container text-center">
+        <form method="POST" action="{{ route('students.store') }}">
 
+            {{-- Token --}}
+            @csrf
 
-            <form method="POST" action="{{ route('students.store') }}">
+            <div class="row justify-content-center">
+                <h1 class="mb-3">Aggiungi Studente</h1>
 
-                {{-- Token --}}
-                @csrf
-
-                <div class="row justify-content-center">
-                    <h1 class="mb-3">ADD Student</h1>
-
-                    {{-- Nome Studente --}}
-                    <div class="mb-3 col-6">
-                        <label class="text-start" for="name">Nome</label>
-                        <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}"
-                            autofocus>
-                    </div>
-
-                    {{-- Cognome Studente --}}
-                    <div class="mb-3 col-6">
-                        <label class="form-label" for="surname">Cognome</label>
-                        <input type="text" id="surname" name="surname" class="form-control"
-                            value="{{ old('surname') }}">
-                    </div>
-
+                {{-- Nome Studente --}}
+                <div class="mb-3 col-6">
+                    <label class="text-start" for="name">Nome</label>
+                    <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}"
+                        autofocus>
                 </div>
-                <button class="btn btn-success">Add</button>
-            </form>
 
-        </div>
+                {{-- Cognome Studente --}}
+                <div class="mb-3 col-6">
+                    <label class="form-label" for="surname">Cognome</label>
+                    <input type="text" id="surname" name="surname" class="form-control" value="{{ old('surname') }}">
+                </div>
+
+            </div>
+            <button class="btn btn-success">Aggiungi Studente</button>
+        </form>
 
     </div>
 @endsection
