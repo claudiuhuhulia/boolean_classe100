09b5337942a0ac9b17f40b4cf433193db0136a4a
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-08-30 12:45:27 +0200

    fixed StudentSeeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $new_student = new Student();
+        $new_student->name = 'pippo';
+        $new_student->surname = 'pluto';
+        $new_student->save();
     }
 }
